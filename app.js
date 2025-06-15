b7aea6f18f30913bb9f3f2632838aca811b01cb9
arshitgajera24 <arshitgajera24@gmail.com>
2025-06-15 12:22:02 +0530

    initial commit

--- app.js
+++ app.js
@@ -106,6 +106,7 @@ const server = createServer(async (req,res) => {
     }
 })
 
-server.listen(PORT, () => {
-    console.log(`Server Running at http://localhost:${PORT}`);
-})
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server Running at http://0.0.0.0:${PORT}`);
+});
+
