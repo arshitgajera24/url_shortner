9fbe43316e7b65e7ee781ee2ad333cf9014009e4
arshitgajera24 <arshitgajera24@gmail.com>
2025-09-16 19:40:21 +0530

    url shortner

--- config/db.js
+++ config/db.js
@@ -1,3 +1,11 @@
+import mysql from "mysql2/promise";
 import { drizzle } from "drizzle-orm/mysql2";
 
-export const db = drizzle(process.env.DATABASE_URL);
+const client = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+export const db = drizzle(client);
